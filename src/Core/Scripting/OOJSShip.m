d0c86d5aa886c1022d9cc375b50103ddf639beaa
AnotherCommander <another_commander@oolite.org>
2015-11-17 21:47:58 +0100

    Fixed rare case of not being able to set max cargo space capacity via JS (thanks ocz) - ported to Maintenance.

--- src/Core/Scripting/OOJSShip.m
+++ src/Core/Scripting/OOJSShip.m
@@ -1375,7 +1375,13 @@ static JSBool ShipSetProperty(JSContext *context, JSObject *this, jsid propID, J
 			if (JS_ValueToInt32(context, *value, &iValue))
 			{
 				if (iValue < 0)  iValue = 0;
-				if ((OOCargoQuantity)iValue < [entity maxAvailableCargoSpace] - [entity availableCargoSpace])
+				// OXPs using equipment with weight requirements may make us end up with more allocated
+				// cargo space than what we have available. Do not let iValue become negative.
+				if ([entity maxAvailableCargoSpace] < [entity availableCargoSpace])
+				{
+					iValue = 0;
+				}
+				else if ((OOCargoQuantity)iValue < [entity maxAvailableCargoSpace] - [entity availableCargoSpace])
 				{
 					iValue = [entity maxAvailableCargoSpace] - [entity availableCargoSpace];
 				}
